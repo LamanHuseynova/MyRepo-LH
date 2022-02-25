85a817fbba85b14544e8b88c855a99c96a7f21b4
lemanhuseynova <laman_hv@yahoo.com>
2022-02-25 15:18:02 -0600

    My new message to commit!

--- src/MyJavaClass2022.java
+++ src/MyJavaClass2022.java
@@ -2,5 +2,7 @@ public class MyJavaClass2022 {
 
     public static void main(String[] args) {
         System.out.println("25th of February");
+        String name = "Laman";
+        System.out.println(name);
     }
 }
